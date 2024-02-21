37fdc07bb163f63436cbc95ee81c30873cb06bfc
Nikhil-Yadav-73 <nikhilyadavwork73@gmail.com>
2024-02-21 21:44:31 +0530

    0.0.0.01

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,12 @@
 console.log("Nikhil");
 logKaro = "Vscode se likha hai"
 console.log(logKaro);
+
+let a = "23"
+console.log(a)
+console.log(typeof a);
+a += 6;
+console.log(a)
+let b = Number(a)
+console.log(b)
+console.log(typeof(b))
